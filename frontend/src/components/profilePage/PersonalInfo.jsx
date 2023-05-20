d51f3b999c566c04b3f17cac6cbd6da5d018d982
amirreza rostami <amirreza.rostami.0073@gmail.com>
2023-05-20 16:54:27 +0330

    finished change profile section

--- frontend/src/components/profilePage/PersonalInfo.jsx
+++ frontend/src/components/profilePage/PersonalInfo.jsx
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 import { EmailModal, FullNameModal, PasswordModal, PhoneModal, WalletModal } from "../utils/ProfileModals";
 import config from "../../services/config.json";
-import axios from 'axios';
+import ContextApi from '../../services/ContextApi';
+
 
 const PersonalInfo = ({ userData }) => {
 
     const [profileData, setProfileData] = useState();
+    const context = useContext(ContextApi);
 
     const profileEditApi = async () => {
-        console.log(profileData);
+        const formData = new FormData();
+        formData.append("profile", profileData);
+
+        await axios.put(`${config.domain}/api/user/editProfile/${userData._id}`, formData, { headers: { "x-auth-token": localStorage.getItem("token") } }).then(res => {
+            setProfileData(undefined);
+            toast.success(`Profile changed`, {
+                position: "bottom-right",
+                theme: "light",
+                closeOnClick: true
+            })
+            context.setUserData(res.data.user);
+        }).catch(err => {
+            if (err.message == "Request failed with status code 401") {
+                toast.error(`You are not allowed to change the information!`, {
+                    position: "bottom-right",
+                    theme: "light",
+                    closeOnClick: true
+                })
+                return console.log(err);
+            }
+            toast.error(`Something went wrong!`, {
+                position: "bottom-right",
+                theme: "light",
+                closeOnClick: true
+            })
+            console.log(err);
+        })
     }
 
     return (
@@ -21,13 +51,13 @@ const PersonalInfo = ({ userData }) => {
 
                     <div className="position-relative">
                         <img src={userData ? `${config.domain}/${userData.profile}` : null} className="img-fluid rounded-circle" id='info-profile' alt="" />
-                        <div className="mt-3">
+                        <div className="mt-3 d-flex align-items-baseline">
                             {/* <input type="file" id="actual-btn" hidden onChange={e => setAvatarData(e.target.files[0])} /> */}
                             <input type="file" id="actual-btn" hidden onChange={e => setProfileData(e.target.files[0])} />
                             {/* <label for="actual-btn" className='w-100' id='register-avatar'>{avatarData ? avatarData.name : "Choose File"}</label> */}
                             <label for="actual-btn" className='btn' id='change-profile'>{profileData ? profileData.name : "Edit Profile"}</label>
-                            <label for="actual-btn" className='btn ml-2' id='save-profile'><i class="fa-regular fa-user-check"></i></label>
                             {/* <span style={{ fontSize: "1rem" }}>Please select a profile picture</span> */}
+                            {profileData == undefined ? null : <div className='btn ml-2' id='save-profile' onClick={profileEditApi}><i class="fa-regular fa-user-check"></i></div>}
                         </div>
                     </div>
                     <div className="dropdown-divider my-3"></div>
